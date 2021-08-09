673cba05453811933366dddb7c268d6a6a5ee8fe
Connor Dunlop <cdu51@uclive.ac.nz>
2021-08-09 13:31:27 +1200

    Fixed broken test to work with Maven's old junit version.

--- src/test/java/seng202/group2/ConnorTestClassTest.java
+++ src/test/java/seng202/group2/ConnorTestClassTest.java
@@ -1,11 +1,10 @@
 package seng202.group2;
 
+
+
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
 import junit.framework.TestCase;
 
@@ -13,26 +12,27 @@ public class ConnorTestClassTest extends TestCase {
 	private final PrintStream systemOut = System.out;
 	private final ByteArrayOutputStream testStream = new ByteArrayOutputStream();
 	
-	@BeforeEach
+	
 	public void captureSystemOut() {
 		System.out.println("Comandeering output stream");
 		System.setOut(new PrintStream(testStream));
 	}
 	
-	@Test
-	void testCountTo10() {
+	
+	public void testCountTo10() {
+		captureSystemOut();
+		
 		ConnorTestClass c = new ConnorTestClass();
-		
 		c.countTo10();
-		
 		assertEquals("12345678910", testStream.toString().trim());
+		
+		returnSystemOut();
 	}
 	
-	@AfterEach
+	
 	public void returnSystemOut() {
 		System.setOut(systemOut);
 		System.out.println("Output returned to system stream");
-		ConnorTestClass c = new ConnorTestClass();
 	}
 	
 }
