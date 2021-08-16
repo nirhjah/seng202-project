e92130a019e6e349a02946c92406fda8860f9c36
Connor Dunlop <cdu51@uclive.ac.nz>
2021-08-17 01:31:40 +1200

    Tmp. main implementation for import and display of records for debugging

--- src/main/java/seng202/group2/ApplicationEnvironment.java
+++ src/main/java/seng202/group2/ApplicationEnvironment.java
@@ -1,10 +1,37 @@
 package seng202.group2;
 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
 // TODO: implement class
 public class ApplicationEnvironment {
-
+	
 	public static void main(String[] args) {
+		String fileName = args[0];
 		
+		DataImporter importer = null;
+		
+		try {
+			importer = DataImporter.getImporter(fileName);
+			
+		} catch (UnsupportedFileTypeException e) {
+			System.out.println("The specified file is of an unsupported file type.");
+			System.exit(1);
+		} catch (FileNotFoundException e) {
+			System.out.println("Could not find the specified file.");
+			System.exit(1);
+		}
+		
+		CrimeData crimeData = importer.importAllRecords();
+		
+		ArrayList<CrimeRecord> records = crimeData.getCrimeRecords();
+		
+		for (CrimeRecord record : records) {
+			System.out.println("");
+			System.out.println(record + "\n");
+		}
+		
+		importer.close();
 	}
 
 }
