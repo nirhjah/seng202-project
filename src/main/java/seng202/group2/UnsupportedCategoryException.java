4daf5a32408fac826f9dc2995a9e9cbef119401f
Connor Dunlop <cdu51@uclive.ac.nz>
2021-08-17 21:53:08 +1200

    Added JavaDoc to exceptions.

--- src/main/java/seng202/group2/UnsupportedCategoryException.java
+++ src/main/java/seng202/group2/UnsupportedCategoryException.java
@@ -1,5 +1,12 @@
 package seng202.group2;
 
+/**
+ * This exception type is thrown when a process tries to perform an operation on or using
+ * data from a category that is unsupported, or when trying to add data to an attribute
+ * corresponding to a DataCategory which is unsupported or does not exist. 
+ * @author Connor Dunlop
+ *
+ */
 public class UnsupportedCategoryException extends Exception {
 	
 	public UnsupportedCategoryException(String category) {
--- src/main/java/seng202/group2/UnsupportedFileTypeException.java
+++ src/main/java/seng202/group2/UnsupportedFileTypeException.java
@@ -1,5 +1,11 @@
 package seng202.group2;
 
+/**
+ * This exception type is thrown when trying to import a file which
+ * has an unsupported file format.
+ * @author Connor Dunlop
+ *
+ */
 public class UnsupportedFileTypeException extends Exception {
 
 	public UnsupportedFileTypeException(String suffix) {
