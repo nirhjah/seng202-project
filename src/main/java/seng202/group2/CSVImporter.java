16e7ba2a3e495c1f20e521cf9c467b9eda100020
Connor Dunlop <cdu51@uclive.ac.nz>
2021-08-17 21:49:08 +1200

    Completed initial implementation of CSVImporter, including JavaDoc.
    Changed the way a file suffix is retrieved.
    Switched to reading data from CSV files using OpenCSV.

--- src/main/java/seng202/group2/CSVImporter.java
+++ src/main/java/seng202/group2/CSVImporter.java
@@ -1,139 +1,104 @@
 package seng202.group2;
 
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
-import java.util.Scanner;
 
+import com.opencsv.CSVReader;
+
+/**
+ * A crime data importer used to import crime data from CSV files.
+ * Uses OpenCSV for parsing CSV files.
+ * 
+ * @author Connor Dunlop
+ *
+ */
 public class CSVImporter extends DataImporter {
 	
-	private HashMap<Integer, DataCategory> categoryMap = new HashMap<Integer, DataCategory>();
+	/** A CSV file reader from OpenCSV used to read in crime data from a CSV file. */
+	private CSVReader fileReader;
+	/**
+	 * A map of the indices of the different DataCategory's of a crime record in each line of data.
+	 * Used when parsing data into a CrimeRecord to keep track of which column represents which category of data.
+	 * */
+	private HashMap<Integer, DataCategory> categoryMap;
 	
-	public CSVImporter(String fileName) throws FileNotFoundException {
-		super(fileName);
+	/**
+	 * Constructs a CSVImporter, setting the file path and instantiating the file reader.
+	 * 
+	 * @param file A File instance constructed from the path of the file to be imported from.
+	 * @throws FileNotFoundException If there was no file found at the supplied file path
+	 */
+	public CSVImporter(File file) throws FileNotFoundException {
+		super(file);
 		
-		parseCategories();
+		// Make a new CSV file parser from OpenCSV
+		fileReader = new CSVReader(new FileReader(file));
 	}
 	
-	private void parseCategories() {
-		String headerLine = fileScanner.nextLine();
-		String[] categories = headerLine.split(",");
+	@Override
+	public void close() throws IOException {
+		fileReader.close();
+	}
+	
+	/**
+	 * Parses the first line of the CSV file to determine what DataCategory's are included in the file.
+	 * If any additional categories of data are included in the file, they are ignored.
+	 * If any DataCategory's are not included in the file, or there is data for some crime records missing
+	 * from a category, then when parsing crime data from the file, the CrimeRecord(s) will take on 
+	 * the default values for those categories.
+	 * 
+	 * @throws IOException If an error occurs while parsing the CSV file.
+	 */
+	private void parseCategories() throws IOException {
 		
+		categoryMap = new HashMap<Integer, DataCategory>();
+		String[] categories = {};
+		
+		try {
+			// Try to parse header line into String[]
+			categories = fileReader.readNextSilently();
+		} catch (IOException error) {
+			throw error;
+		}
+		
+		// Map supported DataCategory's to their index in a row
 		for (int i = 0; i < categories.length; i++) {
 			DataCategory category;
 			try {
+				// Try to determine DataCategory of column from header string
 				category = DataCategory.getCategoryFromString(categories[i]);
 				categoryMap.put(i, category);
 			} catch (UnsupportedCategoryException e) {
-				System.out.println("Could not import unsupported data category: " + categories[i]);
+				// If DataCategory could not be determined from header string
+				// It is assumed to be an unsupported category of data
+				System.out.println("Did not import unsupported data category: " + categories[i]);
 			}
 			
 		}
 	}
 	
-	private Object parseValue(DataCategory category, String value) {
-		switch (category) {
-		case CASE_NUM:
-		case BLOCK:
-		case IUCR:
-		case PRIMARY_DESCRIPTION:
-		case SECONDARY_DESCRIPTION:
-		case LOCATION_DESCRIPTION:
-		case FBI_CODE:
-			return value;
-		
-		case DATE:
-			return parseDate(value);
-		case ARREST:
-			return parseArrest(value);
-		case DOMESTIC:
-			return parseDomestic(value);
-		case BEAT:
-			return parseBeat(value);
-		case WARD:
-			return parseWard(value);
-		case LATITUDE:
-			return parseLatitude(value);
-		case LONGITUDE:
-			return parseLongitude(value);
-		default:
-			throw new IllegalArgumentException();
-		}
-	}
-	
-	private Calendar parseDate(String date) {
-		// 11/23/2020 03:05:00 PM
-		String[] splitDate = date.split(" ");
-		
-		// Split date/time into date and time
-		date = splitDate[0];
-		String time = splitDate[1];
-		String am_pm = splitDate[2];
-		
-		// Get date in day, month, year
-		splitDate = date.split("/");
-		int day = Integer.parseInt(splitDate[1]);
-		int month = Integer.parseInt(splitDate[0]);
-		int year = Integer.parseInt(splitDate[2]);
-		
-		// Get time in hour, minute, second
-		String[] splitTime = time.split(":");
-		int hour = (am_pm == "PM") ?  Integer.parseInt(splitTime[0]) + 12 : Integer.parseInt(splitTime[0]);
-		int minute = Integer.parseInt(splitTime[1]);
-		int second = Integer.parseInt(splitTime[2]);
-		
-		// Return a calender object representing date/time
-		return new GregorianCalendar(year, month, day, hour, minute, second);
-	}
-	
-	private boolean parseArrest(String arrest) {
-		return (arrest == "Y") ? true : false;
-	}
-	
-	private boolean parseDomestic(String domestic) {
-		return (domestic == "Y") ? true : false;
-	}
-	
-	private Object parseBeat(String beat) {
-		if (beat == "")
-			return null;
-		return Short.parseShort(beat);
-	}
-	
-	private Object parseWard(String ward) {
-		if (ward == "")
-			return null;
-		return Short.parseShort(ward);
-	}
-	
-	private Object parseLatitude(String latitude) {
-		if (latitude == "")
-			return null;
-		return Float.parseFloat(latitude);
-	}
-	
-	private Object parseLongitude(String longitude) {
-		if (longitude == "")
-			return null;
-		return Float.parseFloat(longitude);
-	}
-
-
-	
-	private CrimeRecord parseRecord(String recordLine) {
-		String[] values = recordLine.split(",", -1);
-		
+	/**
+	 * Parses an array of string values collected from a line of the CSV file into a CrimeRecord.
+	 * 
+	 * @param values The values of attributes of a crime record, in the order corresponding to that of the headers.
+	 * @return A CrimeRecord whose attributes have been set to the parsed values supplied.
+	 */
+	private CrimeRecord parseRecord(String[] values) {
 		CrimeRecord record = new CrimeRecord();
 		
+		// For each DataCategory included in the file
 		for (int i : categoryMap.keySet()) {
+			DataCategory category = categoryMap.get(i);
 			try {
-				DataCategory category = categoryMap.get(i);
-				Object value = parseValue(category, values[i]);
+				// Try to parse the value corresponding to that DataCategory into the CrimeRecord
+				Object value = category.parseString(values[i]);
 				category.setCategoryValue(record, value);
 			} catch (IllegalArgumentException e) {
-				// TODO Define error behaviour
-				e.printStackTrace();
+				// An error occurred while trying to parse the value
+				System.out.println("Error parsing value: " + values[i] + " for category: " + category);
 			}
 		}
 		
@@ -141,17 +106,19 @@ public class CSVImporter extends DataImporter {
 	}
 	
 	@Override
-	public CrimeData importAllRecords() {
+	public CrimeData importAllRecords() throws IOException {
+		// Parse the header row if it has not been parsed yet.
+		if (categoryMap == null)
+			parseCategories();
+		
 		CrimeData crimeData = new CrimeData();
 		
-		while (fileScanner.hasNextLine()) {
-			String recordLine = fileScanner.nextLine();
-			crimeData.addRecord(parseRecord(recordLine));
+		// Read all crime records into crimeData object
+		String[] values;
+		while ((values = fileReader.readNextSilently()) != null) {
+			crimeData.addRecord(parseRecord(values));
 		}
 		
 		return crimeData;
 	}
-	
-	
-
 }
