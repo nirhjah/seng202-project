09f52afcbebf82128685fa9f5446583ae9d3b026
= <mwe50@uclive.ac.nz>
2021-08-18 19:38:30 +1200

    Completed a rough start to the SQLite version. Tests have not been implemented for all classes, so I cannot gurantee everything is working perfectly.
    After reviewing this system I would continue further.

---
+++ src/main/java/seng202/group2/Test.java
@@ -0,0 +1,35 @@
+package seng202.group2;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ * THIS IS A TEMPORARY TESTING CLASS. DO NOT HAVE THIS WHEN MERGING WITH MASTER
+ */
+
+public class Test {
+    public static  void main(String[] args) throws SQLException, ClassNotFoundException {
+        ActiveData activeData = new ActiveData();
+        ResultSet results;
+
+        DBMS.clearDB();
+
+        //Add some data
+        DBMS.addRecord("TEST1", "2001/12/12 03:40:12 PM", "Block1", "IUCR1", "pDesc1",  "SDec1",
+                "lDesc1", false, false, (short)1, (short)1, "fbiCode1", 1.0f, 1.0f);
+        DBMS.addRecord("TEST2", "2001/12/12 03:40:12 PM", "Block2", "IUCR2", "pDesc2",  "SDec2",
+                "lDesc2", false, false, (short)2, (short)2, "fbiCode2", 2.0f, 2.0f);
+        DBMS.addRecord("TEST2", "2001/12/12 03:40:12 PM", "Block2", "IUCR2", "pDesc2",  "SDec2",
+                "lDesc2", false, false, (short)2, (short)2, "fbiCode2", 2.0f, 2.0f);
+        DBMS.addRecord("TEST3", "2001/12/12 03:40:12 PM", "Block3", "IUCR3", "pDesc3",  "SDec3",
+                "lDesc3", false, false, (short)3, (short)3, "fbiCode3", 3.0f, 3.0f);
+
+        activeData.addFilter(FilterType.EQ.createFilter(DataCategory.CASE_NUM, "TEST2"));
+        ArrayList<CrimeRecord> records =  activeData.getActiveRecords();
+
+        for (CrimeRecord record: records) {
+            System.out.println(record.getCaseNum());
+        }
+    }
+}
