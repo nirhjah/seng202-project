888e817ada51dec0e449307a7ee1958548d50bbf
Connor Dunlop <cdu51@uclive.ac.nz>
2021-08-30 02:53:53 +1200

    Added null argument checks.

--- src/main/java/seng202/group2/datacategories/Domestic.java
+++ src/main/java/seng202/group2/datacategories/Domestic.java
@@ -12,21 +12,30 @@ public class Domestic extends DataCategory {
 
 	@Override
 	public void setCategoryValue(CrimeRecord record, Object data) {
+		if (record == null)
+			throw new IllegalArgumentException("Cannot set attribute value of null record.");
+		
 		if (data == null)
 			record.setDomestic(null);
 		else if (data instanceof Boolean)
 			record.setDomestic((Boolean) data);
 		else
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("Data was of an incorrect type for Domestic.");
 	}
 
 	@Override
 	public Boolean getCategoryValue(CrimeRecord record) {
+		if (record == null)
+			throw new IllegalArgumentException("Cannot get attribute value of null record.");
+		
 		return record.getDomestic();
 	}
 
 	@Override
 	public Boolean parseString(String value) {
+		if (value == null)
+			throw new IllegalArgumentException("Cannot parse null string.");
+		
 		if (value == "")
 			return null;
 		else if (value.equals("Y"))
