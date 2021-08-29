f79cbe7e23c1e7abd80b78f69cd3daa106cc4055
Connor Dunlop <cdu51@uclive.ac.nz>
2021-08-30 03:47:40 +1200

    Added null argument checks, and longitude boundary checks when parsing.

--- src/main/java/seng202/group2/datacategories/Longitude.java
+++ src/main/java/seng202/group2/datacategories/Longitude.java
@@ -12,26 +12,39 @@ public class Longitude extends DataCategory {
 
 	@Override
 	public void setCategoryValue(CrimeRecord record, Object data) {
+		if (record == null)
+			throw new IllegalArgumentException("Cannot set attribute value of null record.");
+		
 		if (data == null)
 			record.setLongitude(null);
 		else if (data instanceof Float)
 			record.setLongitude((Float) data);
 		else
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("Data was of an incorrect type for Longitude.");
 	}
 
 	@Override
 	public Float getCategoryValue(CrimeRecord record) {
+		if (record == null)
+			throw new IllegalArgumentException("Cannot get attribute value of null record.");
+		
 		return record.getLongitude();
 	}
 
 	@Override
 	public Float parseString(String value) {
-		if (value == "")
+		if (value == null)
+			throw new IllegalArgumentException("Cannot parse null string.");
+		else if (value == "")
 			return null;
 
 		try {
-			return Float.parseFloat(value);
+			Float longitude = Float.parseFloat(value);
+			
+			if (!(-180.0f <= longitude && longitude <= 180.0f))
+				throw new IllegalArgumentException("Latitude out of bounds.");
+			
+			return longitude;
 		} catch (NumberFormatException error) {
 			throw new IllegalArgumentException(error);
 		}
