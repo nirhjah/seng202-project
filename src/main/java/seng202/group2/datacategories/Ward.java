673e0d98cd906cfc08d369764403c130868ca4b7
Connor Dunlop <cdu51@uclive.ac.nz>
2021-08-30 03:16:08 +1200

    Added null argument checks.

--- src/main/java/seng202/group2/datacategories/Ward.java
+++ src/main/java/seng202/group2/datacategories/Ward.java
@@ -12,22 +12,30 @@ public class Ward extends DataCategory {
 
 	@Override
 	public void setCategoryValue(CrimeRecord record, Object data) {
+		if (record == null)
+			throw new IllegalArgumentException("Cannot set attribute value of null record.");
+		
 		if (data == null)
 			record.setWard(null);
 		else if (data instanceof Short)
 			record.setWard((Short) data);
 		else
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("Data was of an incorrect type for Ward.");
 	}
 
 	@Override
 	public Short getCategoryValue(CrimeRecord record) {
+		if (record == null)
+			throw new IllegalArgumentException("Cannot get attribute value of null record.");
+		
 		return record.getWard();
 	}
 
 	@Override
 	public Short parseString(String value) {
-		if (value == "")
+		if (value == null)
+			throw new IllegalArgumentException("Cannot parse null string.");
+		else if (value == "")
 			return null;
 
 		try {
