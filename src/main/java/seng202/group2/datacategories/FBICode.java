a6076504b94c0b0325b74a19feab5dad1b50b4bb
Connor Dunlop <cdu51@uclive.ac.nz>
2021-08-29 22:59:34 +1200

    Added null argument checking.

--- src/main/java/seng202/group2/datacategories/FBICode.java
+++ src/main/java/seng202/group2/datacategories/FBICode.java
@@ -1,9 +1,6 @@
 package seng202.group2.datacategories;
 
 import seng202.group2.CrimeRecord;
-import seng202.group2.NIBRSCode;
-import seng202.group2.NIBRSCodeDictionary;
-
 /**
  * FBI crime code assigned to the crime incident.
  * Used to categorize crime incidents by the type of crime that occurred.
@@ -15,22 +12,30 @@ public class FBICode extends DataCategory {
 
 	@Override
 	public void setCategoryValue(CrimeRecord record, Object data) {
+		if (record == null)
+			throw new IllegalArgumentException("Cannot set attribute value of null record.");
+		
 		if (data == null)
 			record.setFbiCode(null);
 		else if (data instanceof String)
 			record.setFbiCode((String) data);
 		else
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("Data was of an incorrect type for FBICode.");
 	}
 
 	@Override
 	public String getCategoryValue(CrimeRecord record) {
+		if (record == null)
+			throw new IllegalArgumentException("Cannot get attribute value of null record.");
+		
 		return record.getFbiCode();
 	}
 
 	@Override
 	public String parseString(String value) {
-		if (value == "")
+		if (value == null)
+			throw new IllegalArgumentException("Cannot parse null string.");
+		else if (value == "")
 			return null;
 		while (value.startsWith("0"))
 			value = value.replaceFirst("0", "");
