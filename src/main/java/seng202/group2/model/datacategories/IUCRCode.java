41354441ebdcfd196b64e1aaa1d09de21373396d
Connor Dunlop <cdu51@uclive.ac.nz>
2021-09-06 18:44:25 +1200

    IUCRCode now only stores the IUCR code, not descriptions.

--- src/main/java/seng202/group2/model/datacategories/IUCRCode.java
+++ src/main/java/seng202/group2/model/datacategories/IUCRCode.java
@@ -12,24 +12,7 @@ import seng202.group2.model.CrimeRecord;
 public class IUCRCode extends DataCategory implements Importable {
 	
 	/** The IUCR code */
-	public final String IUCR;
-	/** The primary description of the crime type this code corresponds to */
-	public final String PRIMARY_DESCRIPTION;
-	/** The secondary description of the crime type this code corresponds to */
-	public final String SECONDARY_DESCRIPTION;
-	/** 
-	 * If an IUCR code is an index code, then the crime type represented by this IUCR code
-	 * can be found under a NIBRS offense code, which are codes used by the FBI for collating
-	 * and processing crime data.
-	 */
-	public final Boolean INDEX;
-	
-	public IUCRCode(String iucr, String primaryDescription, String secondaryDescription, Boolean index) {
-		this.IUCR = iucr;
-		this.PRIMARY_DESCRIPTION = primaryDescription;
-		this.SECONDARY_DESCRIPTION = secondaryDescription;
-		this.INDEX = index;
-	}
+	private String iucrCode = null;
 
 	@Override
 	public void setRecordValue(CrimeRecord record, Object data) {
@@ -38,14 +21,14 @@ public class IUCRCode extends DataCategory implements Importable {
 			
 		if (data == null)
 			record.setIucr(null);
-		else if (data instanceof IUCRCode)
-			record.setIucr((IUCRCode) data);
+		else if (data instanceof String)
+			record.setIucr((String) data);
 		else
 			throw new IllegalArgumentException("Data was of an incorrect type for IUCRCode.");
 	}
 
 	@Override
-	public IUCRCode getRecordValue(CrimeRecord record) {
+	public String getRecordValue(CrimeRecord record) {
 		if (record == null)
 			throw new IllegalArgumentException("Cannot get attribute value of null record.");
 		
@@ -53,18 +36,33 @@ public class IUCRCode extends DataCategory implements Importable {
 	}
 
 	@Override
-	public IUCRCode parseString(String value) {
+	public String parseString(String value) {
 		if (value == null)
 			throw new IllegalArgumentException("Cannot parse null string.");
 		else if (value == "")
 			return null;
 		while (value.startsWith("0"))
 			value = value.replaceFirst("0", "");
-		return IUCRCodeDictionary.getCode(value);
+		return value;
 	}
 
 	@Override
 	public String getSQL() {
 		return "IUCR";
+	}
+
+	@Override
+	public void setValue(Object value) {
+		if (value == null)
+			this.iucrCode = null;
+		else if (value instanceof String)
+			this.iucrCode = (String) value;
+		else
+			throw new IllegalArgumentException("Data was of an incorrect type for IUCRCode.");
+	}
+
+	@Override
+	public String getValue() {
+		return this.iucrCode;
 	}
 }
