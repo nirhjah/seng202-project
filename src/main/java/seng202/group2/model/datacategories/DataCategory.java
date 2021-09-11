40ee245d6c8fd1124827ac7b16a89a92d468c14b
Connor Dunlop <cdu51@uclive.ac.nz>
2021-09-12 03:55:58 +1200

    Added documentation to methods, attributes, errors.

--- src/main/java/seng202/group2/model/datacategories/DataCategory.java
+++ src/main/java/seng202/group2/model/datacategories/DataCategory.java
@@ -17,14 +17,31 @@ import java.util.Set;
  */
 public abstract class DataCategory {
 
+	/**
+	 * A set of instances of all DataCategory subtypes found using reflection.
+	 * This set is populated when this class is first accessed during runtime,
+	 * through the static block below.
+	 */
+	private static Set<DataCategory> dataCategories = new HashSet<>();
+
+	/**
+	 * Gets the set of static DataCategory instances of DataCategory subtypes.
+	 *
+	 * @return A set of the instances stored by each DataCategory subtype found using reflection.
+	 */
 	public static final Set<DataCategory> getCategories() {
 		return dataCategories;
 	}
 
-	private static Set<DataCategory> dataCategories = new HashSet<>();
-
+	/**
+	 * Searches for DataCategory subtypes in the datacategories package using reflection.
+	 * Populates dataCategories with the static instances stored by each DataCategory subtype,
+	 * using the getInstance method of each DataCategory to retrieve the stored instance.
+	 */
 	static {
-		Reflections reflections = new Reflections("seng202.group2");
+		System.out.println("Scanning for DataCategory subtypes.");
+
+		Reflections reflections = new Reflections("seng202.group2.model.datacategories");
 		Set<Class<? extends DataCategory>> dataCategoryClasses = reflections.getSubTypesOf(DataCategory.class);
 
 		for (Class<? extends DataCategory> dataCategoryClass : dataCategoryClasses) {
@@ -32,18 +49,26 @@ public abstract class DataCategory {
 				DataCategory categoryInstance = (DataCategory) dataCategoryClass.getMethod("getInstance").invoke(null);
 				dataCategories.add(categoryInstance);
 			} catch (IllegalAccessException e) {
-				e.printStackTrace();
+				System.out.println("Could not get instance of DataCategory " + dataCategoryClass);
+				System.out.println("The method \"getInstance\" of " + dataCategoryClass + " is not accessible.");
 			} catch (InvocationTargetException e) {
-				e.printStackTrace();
+				System.out.println("Could not get instance of DataCategory " + dataCategoryClass);
+				System.out.println("The method \"getInstance\" of " + dataCategoryClass + " threw an error:");
+				System.out.println(e.getCause().toString());
+				e.getCause().printStackTrace();
 			} catch (NoSuchMethodException e) {
-				e.printStackTrace();
+				System.out.println("Could not get instance of DataCategory " + dataCategoryClass);
+				System.out.println("The method \"getInstance\" of " + dataCategoryClass + " does not exist.");
 			}
 		}
+
+		System.out.println("Found DataCategory subtypes: " + dataCategories);
 	}
 	
 	/**
 	 * Sets the attribute of CrimeRecord record, which corresponds to this DataCategory
-	 * enumerator to the value stored in data. Type casting is handled within the method. 
+	 * enumerator to the value stored in data. Type casting is handled within the method.
+	 *
 	 * @param record The CrimeRecord instance whose attribute value is to be set.
 	 * @param data The data to set the CrimeRecord's attribute value to.
 	 * @throws UnsupportedCategoryException 
@@ -53,6 +78,7 @@ public abstract class DataCategory {
 	/**
 	 * Gets the value of the attribute of CrimeRecord record, which corresponds to this DataCategory
 	 * enumerator. Type casting is handled within the method.
+	 *
 	 * @param record The CrimeRecord instance whose attribute value is to be retrieved.
 	 * @return The value of the CrimeRecord's attribute.
 	 * @throws UnsupportedCategoryException 
@@ -62,6 +88,7 @@ public abstract class DataCategory {
 	/**
 	 * Parses a string representing a value of data corresponding to this DataCategory
 	 * into whatever type is used by a CrimeRecord to store data corresponding to this DataCategory.
+	 *
 	 * @param value A value string representing a value of data corresponding to this DataCategory.
 	 * @return The value represented by the value string parsed into the type used to store data corresponding to this DataCategory.
 	 * @throws UnsupportedCategoryException 
@@ -77,11 +104,22 @@ public abstract class DataCategory {
 
 	/**
 	 * Returns true if the DataCategory is known to be represented by the given identifier string, false otherwise.
+	 *
 	 * @param identifier The string used to represent a DataCategory.
 	 * @return true if the DataCategory is known to be represented by the given identifier string, false otherwise.
 	 */
 	public abstract boolean matchesString(String identifier);
-	
+
+	/**
+	 * Returns the first DataCategory instance in dataCategories which is known to be represented by the given
+	 * identifier string.
+	 * The given identifier string is stripped of its whitespace before being tested with each DataCategory subtype
+	 * using the matchesString method of the DataCategory subtype.
+	 *
+	 * @param identifier The string used to represent a DataCategory.
+	 * @return The first DataCategory instance found to be represented by the given identifier string.
+	 * @throws UnsupportedCategoryException If no DataCategory is known to be represented by the identifier string.
+	 */
 	public static DataCategory getCategoryFromString(String identifier) throws UnsupportedCategoryException {
 		if (identifier == null)
 			throw new IllegalArgumentException("No category string specified.");
