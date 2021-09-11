05d84e1e420b176aa34585478f1288361a577778
Connor Dunlop <cdu51@uclive.ac.nz>
2021-09-12 04:10:50 +1200

    Formatted for readability.

--- src/main/java/seng202/group2/model/datacategories/DataCategory.java
+++ src/main/java/seng202/group2/model/datacategories/DataCategory.java
@@ -17,6 +17,30 @@ import java.util.Set;
  */
 public abstract class DataCategory {
 
+	/************************************************************************************************************
+	 * 																											*
+	 * 																											*
+	 * 									Collection of DataCategory subtypes.									*
+	 * 																											*
+	 * 																											*
+	 ************************************************************************************************************/
+
+	/**
+	 * A set of instances of all DataCategory subtypes found using reflection.
+	 * This set is populated when this class is first accessed during runtime,
+	 * using the static method declared below, lookupCategories.
+	 */
+	private static final Set<DataCategory> dataCategories = lookupCategories();
+
+	/**
+	 * Gets the set of static DataCategory instances of DataCategory subtypes.
+	 *
+	 * @return A set of the instances stored by each DataCategory subtype found using reflection.
+	 */
+	public static final Set<DataCategory> getCategories() {
+		return dataCategories;
+	}
+
 	/**
 	 * Searches for DataCategory subtypes in the datacategories package using reflection.
 	 * Returns a set populated with the static instances stored by each DataCategory subtype,
@@ -54,67 +78,6 @@ public abstract class DataCategory {
 	}
 
 	/**
-	 * A set of instances of all DataCategory subtypes found using reflection.
-	 * This set is populated when this class is first accessed during runtime,
-	 * using the static method declared above, lookupCategories.
-	 */
-	private static final Set<DataCategory> dataCategories = lookupCategories();
-
-	/**
-	 * Gets the set of static DataCategory instances of DataCategory subtypes.
-	 *
-	 * @return A set of the instances stored by each DataCategory subtype found using reflection.
-	 */
-	public static final Set<DataCategory> getCategories() {
-		return dataCategories;
-	}
-	
-	/**
-	 * Sets the attribute of CrimeRecord record, which corresponds to this DataCategory
-	 * enumerator to the value stored in data. Type casting is handled within the method.
-	 *
-	 * @param record The CrimeRecord instance whose attribute value is to be set.
-	 * @param data The data to set the CrimeRecord's attribute value to.
-	 * @throws UnsupportedCategoryException 
-	 */
-	public abstract void setRecordValue(CrimeRecord record, Object data) throws UnsupportedCategoryException;
-	
-	/**
-	 * Gets the value of the attribute of CrimeRecord record, which corresponds to this DataCategory
-	 * enumerator. Type casting is handled within the method.
-	 *
-	 * @param record The CrimeRecord instance whose attribute value is to be retrieved.
-	 * @return The value of the CrimeRecord's attribute.
-	 * @throws UnsupportedCategoryException 
-	 */
-	public abstract Object getRecordValue(CrimeRecord record) throws UnsupportedCategoryException;
-	
-	/**
-	 * Parses a string representing a value of data corresponding to this DataCategory
-	 * into whatever type is used by a CrimeRecord to store data corresponding to this DataCategory.
-	 *
-	 * @param value A value string representing a value of data corresponding to this DataCategory.
-	 * @return The value represented by the value string parsed into the type used to store data corresponding to this DataCategory.
-	 * @throws UnsupportedCategoryException 
-	 */
-	public abstract Object parseString(String value) throws UnsupportedCategoryException;
-	
-	/**
-	 * Returns the SQL string format of the category
-	 *
-	 * @return SQL String
-	 */
-	public abstract String getSQL();
-
-	/**
-	 * Returns true if the DataCategory is known to be represented by the given identifier string, false otherwise.
-	 *
-	 * @param identifier The string used to represent a DataCategory.
-	 * @return true if the DataCategory is known to be represented by the given identifier string, false otherwise.
-	 */
-	public abstract boolean matchesString(String identifier);
-
-	/**
 	 * Returns the first DataCategory instance in dataCategories which is known to be represented by the given
 	 * identifier string.
 	 * The given identifier string is stripped of its whitespace before being tested with each DataCategory subtype
@@ -137,16 +100,78 @@ public abstract class DataCategory {
 
 		throw new UnsupportedCategoryException("The string \"" + identifier + "\" could not be resolved to a DataCategory class.");
 	}
+
+	/************************************************************************************************************
+	 * 																											*
+	 * 																											*
+	 * 									Subtype operations on data.												*
+	 * 																											*
+	 * 																											*
+	 ************************************************************************************************************/
 	
+	/**
+	 * Sets the attribute of CrimeRecord record, which corresponds to this DataCategory
+	 * enumerator to the value stored in data. Type casting is handled within the method.
+	 *
+	 * @param record The CrimeRecord instance whose attribute value is to be set.
+	 * @param data The data to set the CrimeRecord's attribute value to.
+	 * @throws UnsupportedCategoryException 
+	 */
+	public abstract void setRecordValue(CrimeRecord record, Object data) throws UnsupportedCategoryException;
+	
+	/**
+	 * Gets the value of the attribute of CrimeRecord record, which corresponds to this DataCategory
+	 * enumerator. Type casting is handled within the method.
+	 *
+	 * @param record The CrimeRecord instance whose attribute value is to be retrieved.
+	 * @return The value of the CrimeRecord's attribute.
+	 * @throws UnsupportedCategoryException 
+	 */
+	public abstract Object getRecordValue(CrimeRecord record) throws UnsupportedCategoryException;
+
 	/**
 	 * Sets the value of the data stored within this DataCategory.
 	 * @param value The value of data to be stored in this category.
 	 */
 	public abstract void setValue(Object value);
-	
+
 	/**
 	 * Gets the value of the data stored within this DataCategory.
 	 * @return The value of the data stored in this category.
 	 */
 	public abstract Object getValue();
+	
+	/**
+	 * Parses a string representing a value of data corresponding to this DataCategory
+	 * into whatever type is used by a CrimeRecord to store data corresponding to this DataCategory.
+	 *
+	 * @param value A value string representing a value of data corresponding to this DataCategory.
+	 * @return The value represented by the value string parsed into the type used to store data corresponding to this DataCategory.
+	 * @throws UnsupportedCategoryException 
+	 */
+	public abstract Object parseString(String value) throws UnsupportedCategoryException;
+
+
+	/************************************************************************************************************
+	 * 																											*
+	 * 																											*
+	 *										Subtype utility methods.											*
+	 * 																											*
+	 * 																											*
+	 ************************************************************************************************************/
+	
+	/**
+	 * Returns the SQL string format of the category
+	 *
+	 * @return SQL String
+	 */
+	public abstract String getSQL();
+
+	/**
+	 * Returns true if the DataCategory is known to be represented by the given identifier string, false otherwise.
+	 *
+	 * @param identifier The string used to represent a DataCategory.
+	 * @return true if the DataCategory is known to be represented by the given identifier string, false otherwise.
+	 */
+	public abstract boolean matchesString(String identifier);
 }
