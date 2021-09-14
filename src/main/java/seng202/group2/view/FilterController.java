4543b36797c0e0aa8529b0910b2269614b837e68
scl113 <scl113@uclive.ac.nz>
2021-09-14 15:13:41 +1200

    Cleaned up the filters UI, the remove all button now works.
    Testing and documentation to do.

--- src/main/java/seng202/group2/view/FilterController.java
+++ src/main/java/seng202/group2/view/FilterController.java
@@ -1,5 +1,8 @@
 package seng202.group2.view;
 
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
@@ -8,6 +11,7 @@ import javafx.scene.control.TextField;
 import seng202.group2.model.DBMS;
 import seng202.group2.model.Filter;
 import seng202.group2.model.FilterType;
+import seng202.group2.model.datacategories.DataCategory;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -16,47 +20,16 @@ import java.util.ResourceBundle;
 
 public class FilterController implements Initializable {
 
+    /** The JavaFX ComboBox that allows the user to select the category for filtering*/
     @FXML private ComboBox<String> categoryComboBox;
     @FXML private ComboBox<String> comparatorsComboBox;
     @FXML private TextField filterValueTextField;
     @FXML private ListView filterListView;
 
-    private HashMap<String, String> categories;
+    private HashMap<String, String> categories = new HashMap<>();;
     private HashMap<String, FilterType> comparators;
-    private HashMap<String, Filter> listedFilters = new HashMap<>();
-
-    /**
-     * Ideally this method lives somewhere else, perhaps in the DataCategories class or something similar.
-     */
-    private void buildCategories()
-    {
-        categories = new HashMap<>();
-        categories.put("Case Number", "caseNum");
-        categories.put("Date", "date");
-        categories.put("Block", "block");
-        categories.put("IUCR", "IUCR");
-        categories.put("Primary Description", "primaryDescription");
-        categories.put("Secondary Description", "secondaryDescription");
-        categories.put("Location Description", "locationDescription");
-        categories.put("Arrest", "arrest");
-        categories.put("Domestic", "domestic");
-        categories.put("Beat", "beat");
-        categories.put("Ward", "ward");
-        categories.put("Fbi Code", "fbiCode");
-        categories.put("Latitude", "latitude");
-        categories.put("Longitude", "longitude");
-    }
-
-    /**
-     * I believe this list is accessible by the enum, but they would need a 'visual' method
-     * this method would need to get the ">" string from the FilterType.EQ type etc.
-     */
-    private void buildComparators()
-    {
-        comparators = new HashMap<String, FilterType>();
-        comparators.put(">", FilterType.GT);
-        comparators.put("=", FilterType.EQ);
-    }
+    private static HashMap<String, Filter> filterDict = new HashMap<>();
+    private static ObservableList<String> listedFilters = FXCollections.observableArrayList();
 
     /**
      * Adds a filter to the filters list in the ActiveData, this method should also show the filter in the listview
@@ -73,29 +46,24 @@ public class FilterController implements Initializable {
                 filterValueTextField.getText());
         DBMS.getActiveData().addFilter(newFilter);
         System.out.println(filterString);
-        listedFilters.put(filterString, newFilter);
-        filterListView.getItems().add(filterString);
+        filterDict.put(filterString, newFilter);
+        listedFilters.add(filterString);
     }
 
     public void removeSelectedFilter()
     {
         String toRemove = (String) filterListView.getSelectionModel().getSelectedItem();
-        DBMS.getActiveData().removeFilter(listedFilters.get(toRemove));
-        listedFilters.remove(toRemove); // This is only required to access the filter object and remove it.
-        filterListView.getItems().remove(toRemove);
+        DBMS.getActiveData().removeFilter(filterDict.get(toRemove));
+        filterDict.remove(toRemove); // This is only required to access the filter object and remove it.
+        listedFilters.remove(toRemove);
     }
 
-    /**
-     * TODO This should be changed to a method in Active data.
-     */
+
     public void removeAllFilters()
     {
-        for (Filter filter: DBMS.getActiveData().getFilters())
-        {
-            DBMS.getActiveData().removeFilter(filter);
-        }
-        filterListView.getItems().clear();
-        filterListView.getItems().setAll(listedFilters.keySet());
+        ArrayList<Filter> filters = DBMS.getActiveData().getFilters();
+        DBMS.getActiveData().clearFilters(true);
+        filterDict.clear();
         listedFilters.clear();
     }
 
@@ -104,26 +72,21 @@ public class FilterController implements Initializable {
      */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        buildCategories();
-        buildComparators();
-        categoryComboBox.getItems().setAll(getCategories().keySet());
-        comparatorsComboBox.getItems().setAll(getComparators().keySet());
+
+        for (DataCategory category: DataCategory.getCategories())
+        {
+            categories.put(category.toString(), category.getSQL());
+        }
+
+        comparators = new HashMap<String, FilterType>();
+        comparators.put(">", FilterType.GT);
+        comparators.put("=", FilterType.EQ);
+
+
+        categoryComboBox.getItems().setAll(categories.keySet());
+        comparatorsComboBox.getItems().setAll(comparators.keySet());
         categoryComboBox.getSelectionModel().select(0);
         comparatorsComboBox.getSelectionModel().select(0);
-    }
-
-    /**
-     * Ideally this method would live in another class to have a more universal access.
-     * @return
-     */
-    private HashMap<String, String> getCategories() {
-        return categories;
-    }
-
-    /**
-     * Ideally this method would live in another class to have a more universal access.
-     */
-    private HashMap<String, FilterType> getComparators() {
-        return comparators;
+        filterListView.setItems(listedFilters);
     }
 }
