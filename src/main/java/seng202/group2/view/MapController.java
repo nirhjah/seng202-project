785521bdd7ce4c954263eeb344f83a847035e8d7
Yiyang (Jessie) Yu <yyu69@uclive.ac.nz>
2021-09-16 12:47:15 +1200

    [TODO] Add WebView Maven Dependency

--- src/main/java/seng202/group2/view/MapController.java
+++ src/main/java/seng202/group2/view/MapController.java
@@ -1,6 +1,9 @@
 package seng202.group2.view;
 
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.web.WebView;
 import seng202.group2.controller.DataObserver;
 
 /**
@@ -11,7 +14,12 @@ import seng202.group2.controller.DataObserver;
 
 public class MapController{
 
-	
-	
-	
+    /** The center panel that hold the visualization of a map. */
+    @FXML private AnchorPane mapPane;
+    @FXML private WebView webView;
+
+
+
+
+
 }
