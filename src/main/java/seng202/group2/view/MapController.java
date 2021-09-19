dbca9ac27a7956f2944366d8a859e96a5b9d888f
Yiyang (Jessie) Yu <yyu69@uclive.ac.nz>
2021-09-20 09:24:57 +1200

    [ERROR] with java: <identifier> expected

--- src/main/java/seng202/group2/view/MapController.java
+++ src/main/java/seng202/group2/view/MapController.java
@@ -2,7 +2,9 @@ package seng202.group2.view;
 
 
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
@@ -16,29 +18,28 @@ import seng202.group2.controller.DataObserver;
 
 public class MapController{
 
-//    /** The center panel that hold the visualization of a map. */
-//    @FXML private AnchorPane mapPane;
-//    @FXML private WebView webView;
-//    private webEngine webEngine;
+    /** The center panel that hold the visualization of a map. */
+    @FXML private AnchorPane mapPane;
+    @FXML private WebView webView;
+    private webEngine webEngine;
+
+    webView.getEngine().load("http://google.com");
+
+
+//    public void start(Stage primaryStage) {
+//        primaryStage.setTitle("JavaFX WebView Example");
 //
-//    webEngine = webView.getEngine();
-//    webEngine.load("http://google.com");
-
-
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("JavaFX WebView Example");
-
-        WebView webView = new WebView();
-
-        webView.getEngine().load("http://google.com");
-
-        VBox vBox = new VBox(webView);
-        Scene scene = new Scene(vBox, 960, 600);
-
-        primaryStage.setScene(scene);
-        primaryStage.show();
-
-    }
+//        WebView webView = new WebView();
+//
+//        webView.getEngine().load("http://google.com");
+//
+//        VBox vBox = new VBox(webView);
+//        Scene scene = new Scene(vBox, 960, 600);
+//
+//        primaryStage.setScene(scene);
+//        primaryStage.show();
+//
+//    }
 
 
 }
