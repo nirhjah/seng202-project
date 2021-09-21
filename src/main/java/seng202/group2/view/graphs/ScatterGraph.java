cb3ca75b0ab20743481fa4c7064f39e599e2bf16
Connor Dunlop <cdu51@uclive.ac.nz>
2021-09-22 05:28:02 +1200

    Added inherited method stubs.

--- src/main/java/seng202/group2/view/graphs/ScatterGraph.java
+++ src/main/java/seng202/group2/view/graphs/ScatterGraph.java
@@ -1,5 +1,28 @@
 package seng202.group2.view.graphs;
 
+import javafx.scene.layout.BorderPane;
+import seng202.group2.model.datacategories.DataClassification;
+
+import java.util.Set;
+
+/**
+ *
+ * @author Connor Dunlop
+ */
 public class ScatterGraph extends Graph {
 
+    @Override
+    public void initialize(BorderPane borderPane) {
+
+    }
+
+    @Override
+    public Set<Field<? extends DataClassification>> getFields() {
+        return null;
+    }
+
+    @Override
+    public void plotGraph() {
+
+    }
 }
