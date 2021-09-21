df963a54972c8f711658f2b499acb799103b70ac
Connor Dunlop <cdu51@uclive.ac.nz>
2021-09-22 03:38:49 +1200

    Partial implementation of BarGraph, including fields and initialization.

--- src/main/java/seng202/group2/view/graphs/BarGraph.java
+++ src/main/java/seng202/group2/view/graphs/BarGraph.java
@@ -1,5 +1,56 @@
 package seng202.group2.view.graphs;
 
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.layout.BorderPane;
+import seng202.group2.model.datacategories.Categorical;
+import seng202.group2.model.datacategories.DataClassification;
+import seng202.group2.model.datacategories.Numerical;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author Connor Dunlop
+ */
 public class BarGraph extends Graph {
 
+    private CategoryAxis xAxis = new CategoryAxis();
+    private NumberAxis yAxis = new NumberAxis();
+
+    private Field<Categorical> xField = Field.newField("X Axis", Categorical.class);
+    private Field<Numerical> yField = Field.newField("Y Axis", Numerical.class);
+
+    BarChart<String, Number> barChart = new BarChart<>(xAxis, yAxis);
+
+    BarGraph(BorderPane borderPane) {
+        barChart.setTitle("Bar Graph");
+        borderPane.setCenter(barChart);
+    }
+
+    @Override
+    public Set<Field<? extends DataClassification>> getFields() {
+        HashSet<Field<? extends DataClassification>> fields = new HashSet<>();
+        fields.add(xField);
+        fields.add(yField);
+        return fields;
+    }
+
+    @Override
+    public void plotGraph() {
+        xAxis.setLabel(xField.getDataCategory().toString());
+        yAxis.setLabel(yField.getDataCategory().toString());
+    }
+
+
+
+
+/*
+    //  plotRecords();
+	        dataseries.setName("Data");
+	        barChart.getData().addAll(dataseries);
+	        */
 }
