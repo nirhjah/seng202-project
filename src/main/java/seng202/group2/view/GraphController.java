119fe458f4cc83e3764f528cefce8bcf39bf4d8f
Connor Dunlop <cdu51@uclive.ac.nz>
2021-09-24 01:23:48 +1200

    Graph types are now responsible for providing their own user interface for selecting fields and settings.

--- src/main/java/seng202/group2/view/GraphController.java
+++ src/main/java/seng202/group2/view/GraphController.java
@@ -1,31 +1,15 @@
 package seng202.group2.view;
 
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
-import javafx.beans.value.ObservableValue;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.chart.BarChart;
-import javafx.scene.chart.CategoryAxis;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.ScatterChart;
-import javafx.scene.chart.XYChart;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
-import javafx.util.Callback;
+import javafx.scene.layout.VBox;
 import seng202.group2.controller.DataObserver;
-import seng202.group2.model.CrimeRecord;
 import seng202.group2.model.DBMS;
-import seng202.group2.model.datacategories.DataCategory;
-import seng202.group2.model.datacategories.DataClassification;
-import seng202.group2.model.datacategories.UnsupportedCategoryException;
-import seng202.group2.view.graphs.Field;
 import seng202.group2.view.graphs.Graph;
 
 
@@ -37,13 +21,14 @@ import seng202.group2.view.graphs.Graph;
 
 public class GraphController extends DataObserver {
 
-	@FXML private BorderPane borderPane;
+	@FXML private BorderPane graphPane;
+	@FXML private BorderPane controlPane;
+	@FXML private VBox optionList = new VBox();
 
 	private Map<String, Class<? extends Graph>> graphTypes = new HashMap<>();
-	@FXML private ComboBox<String> selectGraph;
+	@FXML private ComboBox<String> graphTypeSelector;
 
-	@FXML private ComboBox<DataCategory> xCategory;
-	@FXML private ComboBox<DataCategory> yCategory;
+	private Graph graph;
 
 	/**
 	 * Initialize method
@@ -51,39 +36,35 @@ public class GraphController extends DataObserver {
 	public void initialize() {
 		DBMS.getActiveData().addObserver(this);
 
-		Set<DataCategory> categories = DataCategory.getCategories();
 		for (Class<? extends Graph> graphType : Graph.getGraphTypes()) {
 			graphTypes.put(graphType.toString(), graphType);
 		}
-
-		xCategory.getItems().setAll(categories);
-		yCategory.getItems().setAll(categories);
-		selectGraph.getItems().setAll(graphTypes.keySet());
+		graphTypeSelector.getItems().addAll(graphTypes.keySet());
 	}
 
-	@FXML public void plotGraph() {
-		// Make a new graph of the correct type
-		Graph graph = null;
+	@FXML public void selectGraphType() {
 		try {
-			graph = Graph.newGraph(graphTypes.get(selectGraph.getSelectionModel().getSelectedItem()));
+			graph = Graph.newGraph(graphTypes.get(graphTypeSelector.getSelectionModel().getSelectedItem()));
 		} catch (InstantiationException e) {
 			e.printStackTrace();
 			return;
 		}
 
-		// Initialize it
-		graph.initialize(borderPane);
+		graphPane.setCenter(null);
 
-		// Set the graph's field categories
-		Set<Field<? extends DataClassification>> fields = graph.getFields();
-		for (Field<? extends DataClassification> field : fields) {
-			if (field.name == "X Axis")
-				field.setDataCategory(xCategory.getSelectionModel().getSelectedItem());
-			else if (field.name == "Y Axis")
-				field.setDataCategory(yCategory.getSelectionModel().getSelectedItem());
-		}
+		optionList.getChildren().clear();
+		graph.initialize(graphPane, optionList);
+	}
 
-		// Plot graph
+	@FXML public void hideControlMenu() {
+		controlPane.setVisible(false);
+	}
+
+	@FXML public void showControlMenu() {
+		controlPane.setVisible(true);
+	}
+
+	@FXML public void plotGraph() {
 		graph.plotGraph();
 	}
 
