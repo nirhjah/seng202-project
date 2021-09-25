cb1abf838707836254b09bd16bf0d501f701a135
Connor Dunlop <cdu51@uclive.ac.nz>
2021-09-26 01:36:29 +1200

    Refactored TimeSeriesGraph to use GraphOption classes.

--- src/main/java/seng202/group2/view/graphs/TimeSeriesGraph.java
+++ src/main/java/seng202/group2/view/graphs/TimeSeriesGraph.java
@@ -1,14 +1,11 @@
 package seng202.group2.view.graphs;
 
 import javafx.scene.chart.XYChart;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import seng202.group2.model.CrimeRecord;
 import seng202.group2.model.DBMS;
 import seng202.group2.model.datacategories.DataCategory;
-import seng202.group2.model.datacategories.Numerical;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -84,9 +81,8 @@ public class TimeSeriesGraph extends LineGraph {
         public abstract Number getIntervalValue(Calendar date);
     }
 
-    protected Label intervalTypeLabel = new Label("Interval Type");
-    protected ComboBox<IntervalType> intervalTypeSelector = new ComboBox<>();
-    protected IntervalType intervalType;
+    protected SelectionGraphOption<IntervalType> intervalTypeSelector = new SelectionGraphOption<>("Interval Type");
+
 
     /*************************************************************************************************************
      *                                   Graph and Option Initialization.                                        *
@@ -94,27 +90,15 @@ public class TimeSeriesGraph extends LineGraph {
 
     @Override
     public void initialize(BorderPane graphPane, VBox optionList) {
-        lineChart.setTitle("Line Chart");
+        super.initialize(graphPane, optionList);
+
+        lineChart.setTitle("Time Series Chart");
         graphPane.setCenter(lineChart);
 
-        intervalTypeSelector.getItems().addAll(IntervalType.values());
+        intervalTypeSelector.setItems(IntervalType.values());
 
-        yAxisSelector.getItems().addAll(DataCategory.getCategories(Numerical.class));
-        yAxisSelector.getItems().sort((i, j) -> {
-            return i.toString().compareTo(j.toString());
-        });
-
-        populateOptions(optionList);
-    }
-
-    private void populateOptions(VBox optionList) {
-        optionList.getChildren().clear();
-        optionList.getChildren().addAll(intervalTypeLabel, intervalTypeSelector, ySelectorLabel, yAxisSelector);
-    }
-
-    private void retrieveOptions() {
-        intervalType = intervalTypeSelector.getSelectionModel().getSelectedItem();
-        yCategory = (Numerical) yAxisSelector.getSelectionModel().getSelectedItem();
+        clearOptions();
+        addOptions(intervalTypeSelector, yAxisSelector);
     }
 
 
@@ -124,33 +108,30 @@ public class TimeSeriesGraph extends LineGraph {
 
     @Override
     public void plotGraph() {
-        retrieveOptions();
+        IntervalType intervalType = intervalTypeSelector.getSelectedItem();
+        DataCategory yCategory = yAxisSelector.getSelectedItem();
 
-        if (!isReady())
+        if (intervalTypeSelector.getSelectedItem() == null || yAxisSelector.getSelectedItem() == null)
             throw new NullPointerException("One or more required fields have not been set.");
 
-        DataCategory yCat = (DataCategory) yCategory;
+        // Set Axis labels
         xAxis.setLabel(intervalType.toString());
-        yAxis.setLabel(yCat.toString());
+        yAxis.setLabel(yCategory.toString());
 
+        // Generate data set from crime records
         ArrayList<CrimeRecord> records = DBMS.getActiveData().getActiveRecords();
-
         XYChart.Series<Number, Number> dataSeries = new XYChart.Series<>();
         for (CrimeRecord record : records) {
             Calendar date = record.getDate();
             dataSeries.getData().add(new XYChart.Data<>(
                     intervalType.getIntervalValue(date),
-                    (Number) yCat.getRecordValue(record)
+                    (Number) yCategory.getRecordValue(record)
             ));
         }
 
+        // Plot data on chart
         lineChart.getData().clear();
         lineChart.getData().addAll(dataSeries);
     }
 
-    private boolean isReady() {
-        if (intervalType != null && yCategory != null)
-            return true;
-        return false;
-    }
 }
