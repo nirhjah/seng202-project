3c6f82d3f80ed39e6c9a4e9189815c4f45abee83
Connor Dunlop <cdu51@uclive.ac.nz>
2021-10-03 23:04:22 +1300

    Added getValueString method stub to Exportable interface.

--- src/main/java/seng202/group2/model/datacategories/Exportable.java
+++ src/main/java/seng202/group2/model/datacategories/Exportable.java
@@ -1,4 +1,6 @@
 package seng202.group2.model.datacategories;
 
 /** A basic interface used to mark DataCategory subtypes as being exportable. */
-public interface Exportable extends DataClassification {}
+public interface Exportable extends DataClassification {
+    public String getValueString();
+}
