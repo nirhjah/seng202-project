aa5c2d3d6eb10eeb38a215c45dadb950f4a32c53
Yiyang (Jessie) Yu <yyu69@uclive.ac.nz>
2021-10-06 22:39:52 +1300

    key hidden in .env file in root directory instead of code. map.html script tag src includes key=API_KEY_MATCHER. MapController.java initialize method updated to replace the API_KEY_MATCHER with API_KEY from the .env file

--- src/main/java/seng202/group2/view/MapController.java
+++ src/main/java/seng202/group2/view/MapController.java
@@ -28,10 +28,16 @@ import seng202.group2.model.ActiveData;
 import seng202.group2.model.CrimeRecord;
 import seng202.group2.model.DBMS;
 import seng202.group2.view.graphs.GraphConfigurationDialogController;
+import io.github.cdimascio.dotenv.Dotenv;
+import javafx.concurrent.Worker;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
@@ -72,29 +78,36 @@ public class MapController extends DataObserver implements Initializable {
      *  - Use webEngine to provide access to the document object model of the web page map.html
      */
     public void initialize(URL location, ResourceBundle resources) {
-        webEngine = webView.getEngine();
-        webEngine.load(CamsApplication.class.getClassLoader().getResource("map.html").toExternalForm());
-
+      webEngine = webView.getEngine();
+      try {
+        File map = new File(CamsApplication.class.getClassLoader().getResource("map.html").getFile());
+        String content = new String(Files.readAllBytes(map.toPath()));
+        Dotenv dotenv = Dotenv.load();
+        content = content.replaceFirst("API_KEY_MATCHER", dotenv.get("API_KEY"));
+        webEngine.loadContent(content);
         // Forwards console.log() output from any javascript to System.out
         WebConsoleListener.setDefaultListener((webView, message, lineNumber, sourceId) -> {
-            System.out.println(message + "[at " + lineNumber + "]");
+          System.out.println(message + "[at " + lineNumber + "]");
         });
+      } catch (IOException e) {
+        System.out.println(e.getMessage());
+      }
 
-        // Wait until javascript in map.html has loaded before trying to call functions from there
-        webEngine.getLoadWorker().stateProperty().addListener((ov, oldState, newState) -> {
-            if (newState == Worker.State.SUCCEEDED) {
-                updateRadius();
-                activeDataUpdate();
-            }
-        });
+      // Wait until javascript in map.html has loaded before trying to call functions from there
+      webEngine.getLoadWorker().stateProperty().addListener((ov, oldState, newState) -> {
+        if (newState == Worker.State.SUCCEEDED) {
+          updateRadius();
+          activeDataUpdate();
+        }
+      });
 
-        radiusSlider.valueChangingProperty().addListener((obs, oldVal, newVal) -> {
-            updateRadius();
-        });
+      radiusSlider.valueChangingProperty().addListener((obs, oldVal, newVal) -> {
+        updateRadius();
+      });
 
-        //Connect javascript to this Java class so that it can call methods
-        JSObject win = (JSObject) webEngine.executeScript("window");
-        win.setMember("app", this);
+      //Connect javascript to this Java class so that it can call methods
+      JSObject win = (JSObject) webEngine.executeScript("window");
+      win.setMember("app", this);
     }
 
     /**
